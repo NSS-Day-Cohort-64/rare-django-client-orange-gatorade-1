578fe3e9e7a949d372e41b5d2b34aae7eb1288a3
Chesney Hardin <chesney.hardin@gmail.com>
2023-07-27 15:57:03 -0500

    filter by category

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react"
-import { getPosts } from "../../managers/posts"
+import { getPosts, getPostsByCategory } from "../../managers/posts"
 import { getUsers } from "../../managers/users"
 import { getCategories } from "../../managers/categories"
-import { Link, useNavigate } from "react-router-dom"
+import { Link} from "react-router-dom"
 
 export const PostList = () => {
     const [posts, setPosts] = useState([])
+    const [ filteredPosts, setFilteredPosts ] = useState([])
     const [users, setUsers] = useState([])
     const [categories, setCategories] = useState([])
-    const navigate = useNavigate()
 
 
     useEffect(() => {
@@ -17,12 +17,42 @@ export const PostList = () => {
         getCategories().then(categoriesData => setCategories(categoriesData))
     }, [])
 
+    useEffect(() => {
+        if(posts.length != 0) {
+            setFilteredPosts(posts)
+        }
+    }, [posts])
+
+    const handleInputChange = (event) => {
+        const categoryId = event.target.value
+        if(categoryId != 0) {
+            getPostsByCategory(event.target.value).then(data => setFilteredPosts(data))
+        }
+        else {
+            setFilteredPosts(posts)
+        }
+    }
+
     return (
         <div style={{ margin: "0rem 3rem" }}>
             <h1>Posts</h1>
+            <div className="form-group">
+                <label htmlFor="category">Category: </label>
+                <select name="category" className="form-control"
+                    onChange={handleInputChange}>
+                    <option value="0">Select a category</option>
+                    {
+                    categories.map(category => (
+                        <option key={category.id} value={category.id}>
+                        {category.label}
+                        </option>
+                    ))
+                    }
+                </select>
+            </div>
             <article className="posts">
                 {
-                    posts.map(post => {
+                    filteredPosts.map(post => {
                         const user= users.find(user => user.id === post.user_id) || []
                         const category = categories.find(category => category.id === post.category_id) || []
                         return <section className="post">
