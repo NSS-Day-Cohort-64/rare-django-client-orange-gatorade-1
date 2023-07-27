f0693cfa936ef2b3d5e6cdd17c30260643b9a42f
Daniel Myers <danielm53861991@gmail.com>
2023-07-27 13:49:51 -0500

    Made some changes

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -37,6 +37,8 @@ export const NavBar = ({ token, setToken }) => {
               <Link to="/posts" className="navbar-item">Posts</Link>
               <Link to="/categories" className="navbar-item">Category Management</Link>
               <Link to="/users" className="navbar-item">User Management</Link>
+              <Link to="/postform" className="navbar-item">New Post</Link>
+
 
               </>
               :
