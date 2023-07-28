0fb1f7707c8a8c11edaacfc3a255dd7455e43ea6
Ryan Phillips <ryanmphill@gmail.com>
2023-07-28 11:35:31 -0500

    Added POST request for tags

--- src/managers/TagManager.js
+++ src/managers/TagManager.js
@@ -2,14 +2,3 @@ export const getTags = () => {
   return fetch("http://localhost:8088/tags")
       .then(res => res.json())
 }
-
-export const postTags = (newTag) => {
-  return fetch("http://localhost:8088/tags", {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-      },
-      body: JSON.stringify(newTag)
-  }).then(res => res.json())
-}
