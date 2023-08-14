e5b8626774e1e0d6ba749646e66c09a38be82421
Belle Hollander <bellehollander@gmail.com>
2023-08-14 16:58:30 -0400

    Create a category, view all categories

--- src/managers/posts.js
+++ src/managers/posts.js
@@ -1,56 +1,54 @@
-
 export const getPosts = () => {
-    return fetch("http://localhost:8088/posts")
-        .then(res => res.json())
-}
-
+  return fetch("http://localhost:8000/posts").then((res) => res.json());
+};
 
 export const getPostById = (id) => {
-    return fetch(`http://localhost:8088/posts/${id}`)
-        .then(res => res.json())
-}
+  return fetch(`http://localhost:8088/posts/${id}`).then((res) => res.json());
+};
 
 export const getPostsByCategory = (categoryId) => {
-    return fetch(`http://localhost:8088/posts?category=${categoryId}`)
-        .then(res => res.json())
-}
-
-
+  return fetch(`http://localhost:8088/posts?category=${categoryId}`).then(
+    (res) => res.json()
+  );
+};
 
 export const viewUserPost = ({ token }) => {
-    const userId = parseInt(token);
-    return fetch(`http://localhost:8088/posts?user=${userId}`)
-        .then((res) => res.json());
+  const userId = parseInt(token);
+  return fetch(`http://localhost:8088/posts?user=${userId}`).then((res) =>
+    res.json()
+  );
 };
 
 export const deletePost = (postId) => {
-    return fetch(`http://localhost:8088/posts/${postId}`, {
-        method: "DELETE"
-    });
+  return fetch(`http://localhost:8088/posts/${postId}`, {
+    method: "DELETE",
+  });
 };
 
 export const putPost = (postId, post) => {
-    return fetch(`http://localhost:8088/posts/${postId}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(post)
-    })
-}
+  return fetch(`http://localhost:8088/posts/${postId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(post),
+  });
+};
 
 export const getPostsByUser = (userId) => {
-    return fetch(`http://localhost:8088/posts?user=${userId}`)
-        .then(res => res.json())
-}
+  return fetch(`http://localhost:8088/posts?user=${userId}`).then((res) =>
+    res.json()
+  );
+};
 
 export const getPostsByTitle = (title) => {
-    return fetch(`http://localhost:8088/posts?title=${title}`)
-    .then(res => res.json())
-}
+  return fetch(`http://localhost:8088/posts?title=${title}`).then((res) =>
+    res.json()
+  );
+};
 
 export const getPostsByTag = (tagId) => {
-    return fetch(`http://localhost:8088/posts?tag=${tagId}`)
-    .then(res => res.json())
-}
-
+  return fetch(`http://localhost:8088/posts?tag=${tagId}`).then((res) =>
+    res.json()
+  );
+};
