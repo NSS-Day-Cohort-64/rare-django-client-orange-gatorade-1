11109d13d33521ad3c0eec11308c3934cfee00f0
Ryan Phillips <ryanmphill@gmail.com>
2023-08-15 12:38:53 -0500

    Retrieved current author so that 'manage tags' only appears for authors own posts

--- src/components/posts/PostDetails.js
+++ src/components/posts/PostDetails.js
@@ -2,17 +2,23 @@ import { useNavigate, useParams } from "react-router-dom"
 import { getPostById } from "../../managers/posts"
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import { getCurrentAuthor } from "../../managers/users"
 
 
 export const PostDetails = ({token}) => {
     const { postId } = useParams()
     const [post, setPost] = useState({})
+    const [currentAuthor, setCurrentAuthor] = useState({})
     const navigate = useNavigate()
 
     useEffect(() => {
         if (postId) {
             getPostById(parseInt(postId)).then(PostDetails => setPost(PostDetails))
         }
+    }, [])
+
+    useEffect(() => {
+        getCurrentAuthor().then(data => setCurrentAuthor(data[0]))
     }, [])
 
 
--- src/managers/users.js
+++ src/managers/users.js
@@ -7,3 +7,15 @@ export const getUserById = (id) => {
     return fetch(`http://localhost:8088/users/${id}`)
         .then(res => res.json())
 }
+
+export const getCurrentAuthor = () => {
+    return fetch(`http://localhost:8088/authors?current=true`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`
+        }
+    })
+        .then(res => res.json())
+}
