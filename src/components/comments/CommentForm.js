808eb2fffcb712b76d09dfdd958705d559be0301
Belle Hollander <bellehollander@gmail.com>
2023-08-15 14:27:00 -0400

    you can now created a comment and view all comments on a post

--- src/components/comments/CommentForm.js
+++ src/components/comments/CommentForm.js
@@ -4,66 +4,68 @@ import { useEffect, useState } from "react";
 import { getPostById } from "../../managers/posts";
 
 export const CommentForm = ({ token }) => {
-    const { postId } = useParams()
-    const [ comment, setComment ] = useState({
-        post_id: 0,
-        author_id: 0,
-        content: ""
-    })
-    const [post, setPost] = useState({})
+  const { postId } = useParams();
+  const [comment, setComment] = useState({
+    post_id: 0,
+    author_id: 0,
+    content: "",
+  });
+  const [post, setPost] = useState({});
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (postId) {
-            getPostById(postId).then(PostDetails => setPost(PostDetails))
-        }
-    }, [postId])
+  useEffect(() => {
+    if (postId) {
+      getPostById(postId).then((PostDetails) => setPost(PostDetails));
+    }
+  }, [postId]);
 
-    const handleSaveButtonClick = (event) => {
-        event.preventDefault();
+  const handleSaveButtonClick = (event) => {
+    event.preventDefault();
 
-        const commentToSendToAPI = {
-            post_id: post.id,
-            author_id: parseInt(token),
-            content: comment.content
-        }
-        postComment(commentToSendToAPI)
-            .then(() => {
-                navigate(`/comments/${postId}`);
-            });
+    const commentToSendToAPI = {
+      post_id: post.id,
+      author_id: parseInt(token),
+      content: comment.content,
     };
+    postComment(commentToSendToAPI).then(() => {
+      navigate(`/comments/${postId}`);
+    });
+  };
 
+  return (
+    <form className="commentForm">
+      <h2 className="commentFormHeader">Leave a comment for "{post.title}"</h2>
 
-    return (
-        <form className="commentForm">
-            <h2 className="commentFormHeader">Leave a comment for "{post.title}"</h2>
-
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="commentHTML" className="commentContent">Comment:</label>
-                    <input
-                        required autoFocus
-                        style={{ width: "30rem", height: "3rem" }}
-                        type="text"
-                        className="form-control"
-                        placeholder="Tell us what you really think"
-                        value={comment.content}
-                        onChange={(evt) => {
-                            const copy = {...comment}
-                            copy.content = evt.target.value
-                            setComment(copy)
-                        }}
-                    />
-                </div>
-            </fieldset>
-            <button
-                onClick={(clickEvent) => { handleSaveButtonClick(clickEvent) }}
-                className="btn btn-primary"
-            >
-                Submit
-            </button>
-        
-        </form>
-    )
-}
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="commentHTML" className="commentContent">
+            Comment:
+          </label>
+          <input
+            required
+            autoFocus
+            style={{ width: "30rem", height: "3rem" }}
+            type="text"
+            className="form-control"
+            placeholder="Tell us what you really think"
+            value={comment.content}
+            onChange={(evt) => {
+              const copy = { ...comment };
+              copy.content = evt.target.value;
+              setComment(copy);
+            }}
+          />
+        </div>
+      </fieldset>
+      <button
+        onClick={(clickEvent) => {
+          handleSaveButtonClick(clickEvent);
+        }}
+        className="btn btn-primary"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
--- src/components/posts/UserPost.js
+++ src/components/posts/UserPost.js
@@ -1,45 +1,45 @@
 import { useEffect, useState } from "react";
 import { deletePost, viewUserPost } from "../../managers/posts";
-import { getCategories } from "../../managers/categories"
+import { getCategories } from "../../managers/categories";
 import { Link, useNavigate, useParams } from "react-router-dom";
-
 
 export const UserPost = ({ token }) => {
   const [userPosts, setUserPosts] = useState([]); // Change 'posts' to 'userPosts'
-  const navigate = useNavigate()
-  const { postId } = useParams()
+  const navigate = useNavigate();
+  const { postId } = useParams();
 
   useEffect(() => {
     viewUserPost({ token }).then((postsData) => setUserPosts(postsData)); // Pass token as an object
   }, [token]);
 
-
   const deleteButton = (postId) => {
     const handleDelete = () => {
-      const shouldDelete = window.confirm("Are you sure you want to delete this post?");
+      const shouldDelete = window.confirm(
+        "Are you sure you want to delete this post?"
+      );
       if (shouldDelete) {
         deletePost(postId).then(() => {
-          setUserPosts((prevPosts) => prevPosts.filter((post) => post.id !== postId));
+          setUserPosts((prevPosts) =>
+            prevPosts.filter((post) => post.id !== postId)
+          );
         });
       }
     };
 
-    return (
-      <button onClick={handleDelete}>
-        Delete
-      </button>
-    );
+    return <button onClick={handleDelete}>Delete</button>;
   };
-
 
   const editButton = (post) => {
     return (
-      <button onClick={() => { navigate(`/my-posts/${post.id}/edit`) }}>
+      <button
+        onClick={() => {
+          navigate(`/my-posts/${post.id}/edit`);
+        }}
+      >
         Edit
       </button>
-    )
-  }
-
+    );
+  };
 
   return (
     <div style={{ margin: "0rem 3rem" }}>
@@ -49,9 +49,11 @@ export const UserPost = ({ token }) => {
           return (
             <section className="post" key={post.id}>
               <div>==============================</div>
-              <div>Title: <Link to={`/posts/${post.id}`}>{post.title}</Link></div>
+              <div>
+                Title: <Link to={`/posts/${post.id}`}>{post.title}</Link>
+              </div>
               <div>Author: {post.author.username}</div>
-              <div>Category: {post.category.label}</div>
+              <div>Category: {post.category?.label}</div>
               <footer>{deleteButton(post.id)}</footer>
               <footer>{editButton(post)}</footer>
             </section>
