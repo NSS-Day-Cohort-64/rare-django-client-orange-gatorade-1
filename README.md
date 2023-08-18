0d3faa000c8ab9f0d6073beeaedf37033138d610
Belle Hollander <bellehollander@gmail.com>
2023-08-18 15:48:29 -0400

    read me

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Rare: The Publishing Platform for the Discerning Writer
 
-## Getting Started
-1. Install dependencies: `npm install`
-2. Run the code `npm start`
-<!-- TODO: Update the remaining steps if anything changes -->
-3. With the server also running, check that the login, register, and logout functionality is in working.
-4. This template is using [Bulma](https://bulma.io/documentation) for styling. Take a little bit of time to familiarize yourself with the framework if you would like to continue using it.
+Rare Media Publication is a platform designed for writers and creators to share their thoughts and connect with others in a new and exciting way. Express yourself through posts, engage in discussions, and discover a vibrant community of like-minded individuals. With the ability to tag and categorize your work, as well as subscribe to fellow writers, Rare Media makes it easy to find inspiration and stay connected.
 
+# Primary Features
 
-<!-- TODO: Finish writing the readme -->
+- Users can post, comment on others posts, and filter through posts
+- Admins can manage users posts (with the ability to approve or deny them)
+- Admins can create different tags, reactions, and categories
+- Users can add tags to their posts and update them
+- Users can subscribe and unsubscribe to other users
--- src/components/posts/PostDetails.js
+++ src/components/posts/PostDetails.js
@@ -1,81 +1,99 @@
-import { useNavigate, useParams } from "react-router-dom"
-import { getPostById } from "../../managers/posts"
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import { getCurrentAuthor } from "../../managers/users"
-import { ManagePostTags } from "./ManagePostTags"
+import { useNavigate, useParams } from "react-router-dom";
+import { getPostById } from "../../managers/posts";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getCurrentAuthor } from "../../managers/users";
+import { ManagePostTags } from "./ManagePostTags";
 
+export const PostDetails = ({ token }) => {
+  const { postId } = useParams();
+  const [post, setPost] = useState({});
+  const [postTags, setPostTags] = useState([]);
+  const [viewingOwnPost, setViewingOwnPost] = useState(false);
+  const [editTags, updateEditTags] = useState(false);
+  const navigate = useNavigate();
 
-export const PostDetails = ({token}) => {
-    const { postId } = useParams()
-    const [post, setPost] = useState({})
-    const [postTags, setPostTags] = useState([])
-    const [viewingOwnPost, setViewingOwnPost] = useState(false)
-    const [editTags, updateEditTags] = useState(false)
-    const navigate = useNavigate()
+  const fetchPostDetails = () => {
+    getPostById(parseInt(postId)).then((postDetails) => {
+      setPost(postDetails);
+      setPostTags(postDetails.tags);
+    });
+  };
 
-    const fetchPostDetails = () => {
-        getPostById(parseInt(postId))
-            .then((postDetails) => {
-                setPost(postDetails)
-                setPostTags(postDetails.tags)
-            })
+  useEffect(() => {
+    if (postId) {
+      fetchPostDetails();
     }
-    
-    useEffect(() => {
-        if (postId) {
-            fetchPostDetails()
-        }
-    }, [postId])
+  }, [postId]);
 
-    useEffect(() => {
-        getCurrentAuthor().then((data) => {
-            const currentAuthor = data[0]
-            if (currentAuthor.id === post?.author?.id) {
-                setViewingOwnPost(true)
-            }
-        })
-    }, [post])
+  useEffect(() => {
+    getCurrentAuthor().then((data) => {
+      const currentAuthor = data[0];
+      if (currentAuthor.id === post?.author?.id) {
+        setViewingOwnPost(true);
+      }
+    });
+  }, [post]);
 
+  return (
+    post && (
+      <div style={{ margin: "0rem 3rem" }}>
+        <h1>{post.title}</h1>
+        <article className="postDetails">
+          <img src={post.image_url} />
+          <div>{post.content}</div>
+          <div>Date: {post.publication_date}</div>
+          <div>
+            Author:{" "}
+            <Link to={`/users/${post.author?.id}`}>
+              {post.author?.username}
+            </Link>
+          </div>
+        </article>
+        <section>
+          {viewingOwnPost && !editTags && (
+            <button onClick={() => updateEditTags(true)}>Manage Tags</button>
+          )}
+          {editTags === true ? (
+            <>
+              <ManagePostTags
+                token={token}
+                postId={postId}
+                currentPost={post}
+                originalTagsOnPost={postTags}
+                updateEditTags={updateEditTags}
+                fetchPostDetails={fetchPostDetails}
+              />
+            </>
+          ) : (
+            <div className="tags">
+              {postTags.map((tag) => (
+                <span
+                  className="tag is-primary is-light"
+                  key={`posttag--${tag?.label}`}
+                >
+                  {tag?.label}
+                </span>
+              ))}
+            </div>
+          )}
+        </section>
 
-
-    return (
-        post &&
-        <div style={{ margin: "0rem 3rem" }}>
-            <h1>{post.title}</h1>
-            <article className="postDetails">
-                <img src={post.image_url} />
-                <div>{post.content}</div>
-                <div>Date: {post.publication_date}</div>
-                <div>Author: <Link to={`/users/${post.author?.id}`}>{post.author?.username}</Link></div>
-            </article>
-            <section>
-                {
-                    viewingOwnPost && !editTags &&
-                    <button onClick={() => updateEditTags(true)}>Manage Tags</button>
-                }
-                {
-                    editTags === true
-                            ? <>
-                                <ManagePostTags
-                                    token={token}
-                                    postId={postId}
-                                    currentPost={post}
-                                    originalTagsOnPost={postTags}
-                                    updateEditTags={updateEditTags}
-                                    fetchPostDetails={fetchPostDetails}
-                                />
-                            </>
-                        : <div className="tags">
-                            {
-                                postTags.map((tag) => 
-                                <span className="tag is-primary is-light" key={`posttag--${tag?.label}`}>{tag?.label}</span>)
-                            }
-                        </div>
-                }
-            </section>
-            <button onClick={() => { navigate(`/comments/${postId}`) }}>View Comments</button>
-            <button onClick={() => { navigate(`/commentform/${postId}`) }}>Add Comment</button>
-        </div>
+        <button
+          onClick={() => {
+            navigate(`/comments/${postId}`);
+          }}
+        >
+          View Comments
+        </button>
+        <button
+          onClick={() => {
+            navigate(`/commentform/${postId}`);
+          }}
+        >
+          Add Comment
+        </button>
+      </div>
     )
-}
+  );
+};
